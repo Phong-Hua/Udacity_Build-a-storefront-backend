c2af728b526c9882e8f7d542e370a17d4e878bc6
PhongHua <phonghua1989@hotmail.com>
2021-05-24 21:14:26 +1000

    first commit

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,15 @@ import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
 
 const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+const port : number = parseInt(process.env.PORT as unknown as string)
 
 app.use(bodyParser.json())
 
 app.get('/', function (req: Request, res: Response) {
+    
     res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
+app.listen(port, function () {
+    console.log(`starting app on port ${port}`)
 })
